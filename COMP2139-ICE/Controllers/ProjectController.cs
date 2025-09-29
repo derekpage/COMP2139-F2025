52ad0c0bcc9385ffd197073a6ead80b9d74ee232
Derek Page <derekpage1@gmail.com>
2025-09-29 14:10:50 -0400

    Lab 3 Connected Project Controller to Database

--- COMP2139-ICE/Controllers/ProjectController.cs
+++ COMP2139-ICE/Controllers/ProjectController.cs
@@ -1,3 +1,4 @@
+using COMP2139_ICE.Data;
 using COMP2139_ICE.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -5,13 +6,22 @@ namespace COMP2139_ICE.Controllers;
 
 public class ProjectController : Controller
 {
+    
+    private readonly ApplicationDbContext _context;
+    public ProjectController(ApplicationDbContext context) {
+        _context = context;
+    }
+    
     [HttpGet]
     public IActionResult Index()
     {
+        /*
         var projects = new List<Project>()
         {
             new Project { ProjectId = 1, Name = "Project 1", Description = "First Project" }
         };
+        */
+        var projects = _context.Projects.ToList();
         return View(projects);
     }
 
@@ -30,7 +40,14 @@ public class ProjectController : Controller
     [HttpGet]
     public IActionResult Details(int id)
     {
+        /*
         var project = new Project{ ProjectId = id, Name = "Project " + id, Description = "Details of Project " + id };
+        */
+        var project = _context.Projects.FirstOrDefault(p => p.ProjectId == id);
+        if (project == null)
+        {
+            return NotFound();
+        }
         return View(project);
     }
 }
